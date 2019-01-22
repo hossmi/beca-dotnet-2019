a82776e714b19a05ebcf2be703a6f7c25a33ea25
Soden <f.navarronicolas@gmail.com>
2019-01-22 11:57:49 +0100

    ADD arreglo

--- BusinessCore/NumberTools.cs
+++ BusinessCore/NumberTools.cs
@@ -10,12 +10,26 @@ namespace BusinessCore
     {
         public static long factorial(long n)
         {
-            throw new NotImplementedException();
+            int resultado = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                resultado = resultado * i;
+            }
+            return resultado;
         }
 
         public static int fibonacci(int n)
         {
-            throw new NotImplementedException();
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
         }
     }
 }
