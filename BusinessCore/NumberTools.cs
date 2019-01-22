c4762863573e40671c1aba9d536e2132b00fbbdd
EpicSimple <anton_the@hotmail.es>
2019-01-22 12:40:52 +0100

    Changed flow to non-recursive

--- BusinessCore/NumberTools.cs
+++ BusinessCore/NumberTools.cs
@@ -10,20 +10,33 @@ namespace BusinessCore
     {
         public static long factorial(long n)
         {
-            if (n == 0)
-                return 1;
-            else
-                return n * factorial(n - 1);
+            long fact = 1;
+            if(n != 0)
+            {
+                for(int i= 0; i < n; i++)
+                {
+                    fact = fact * i;
+                };
+            }
+
+            return fact;
         }
 
         public static int fibonacci(int n)
         {
-            if (n == 0)
-                return 0;
-            else if (n == 1)
-                return 1;
-            else
-                return fibonacci(n - 1) + fibonacci(n - 2);
+            int fib = 0;
+            int oldFib = 1;
+            int temp;
+
+            for (int i = 0; i < n; i++)
+            {
+                temp = fib;
+
+                fib = oldFib + fib;
+                oldFib = temp;
+            };
+
+            return fib;
         }
     }
 }
