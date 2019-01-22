e683c2988c8b4987f652f4d06d94a8484c3b9c39
Celiamg <celiamargil@gmail.com>
2019-01-22 11:45:50 +0100

    Modificar NunberTools

--- BusinessCore/NumberTools.cs
+++ BusinessCore/NumberTools.cs
@@ -10,12 +10,38 @@ namespace BusinessCore
     {
         public static int factorial(int n)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if(n == 1)
+            {
+                return 1;
+            }
+
+            int resul = 1;
+            for(int i = 2; i <= n; i++)
+            {
+                resul *= i;
+            }
+            return resul;
         }
 
         public static int fibonacci(int n)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if(n == 0)
+            {
+                return 0;
+            }
+            else if(n == 1)
+            {
+                return 1;
+            }
+
+            //int resul;
+            return fibonacci(n -1) + fibonacci(n - 2);
         }
     }
 }
