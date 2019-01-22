59ec8990ba9ccd2d5b5c593c6a15bdf0bbd2fa9e
EpicSimple <anton_the@hotmail.es>
2019-01-22 11:37:58 +0100

    Implement factorial(), fibonacci()
    # Test error:
    # factorial 15 too big for int

--- BusinessCore/NumberTools.cs
+++ BusinessCore/NumberTools.cs
@@ -10,12 +10,20 @@ namespace BusinessCore
     {
         public static int factorial(int n)
         {
-            throw new NotImplementedException();
+            if (n == 0)
+                return 1;
+            else
+                return n * factorial(n - 1);
         }
 
         public static int fibonacci(int n)
         {
-            throw new NotImplementedException();
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
 }
