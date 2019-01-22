dc58ea3b6b1605afd023ec3e35e90462aa1c0510
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2019-01-22 11:05:42 +0100

    Rename Class1 to NumberTools

---
+++ BusinessCore/NumberTools.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCore
+{
+    public class NumberTools
+    {
+        public static int factorial(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int fibonacci(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
