46191c56f200ade52db4108b302d7c09a10fb1a2
alloret1993 <alloret1993@outlook.com>
2019-01-22 12:53:19 +0100

    test

--- BusinessCore/NumberTools.cs
+++ BusinessCore/NumberTools.cs
@@ -10,12 +10,27 @@ namespace BusinessCore
     {
         public static long factorial(long n)
         {
-            throw new NotImplementedException();
+            long numero, resultado = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                resultado = resultado * i;
+            }
+            return resultado;
         }
 
         public static int fibonacci(int n)
         {
-            throw new NotImplementedException();
-        }
+                int a = 0;
+                int b = 1;
+
+                for (int i = 0; i < n; i++)
+                {
+                    int temp = a;
+                    a = b;
+                    b = temp + b;
+                }
+                return a;
+            }
     }
 }
