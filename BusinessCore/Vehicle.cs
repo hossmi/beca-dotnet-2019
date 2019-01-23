c46da4a7a236506e04f2f6ef3d322b6ab7c61f35
evoblackshark <kilowan2@gmail.com>
2019-01-23 16:56:14 +0100

    Continue

---
+++ BusinessCore/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCore
+{
+    class Vehicle
+    {
+        public bool Electric
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Doors[] balance = new Doors[5]
+        {
+            FrontRight,
+            FrontLeft,
+            RearRight,
+            RearLeft,
+            Trunk,
+        };
+
+    }
+}
+
+}
+
