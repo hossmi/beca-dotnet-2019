9f3b1af2e029bf71202af0e7f3c1f800d69167c6
alloret1993 <alloret1993@outlook.com>
2019-01-24 08:31:28 +0100

    no message

---
+++ CarManagement/Models/Door.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManagement.Models
+{
+    class Door
+    {
+        private bool open = false;
+
+        public void Open()
+        {
+            try
+            {
+               
+                open = true;
+            }
+            catch (Exception)
+            {
+                open = false;
+            }
+        }
+
+        public bool IsOpen
+        {
+            get
+            {
+                return open;
+            }
+
+        }
+    }
+}
