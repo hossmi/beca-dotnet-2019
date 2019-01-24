ac9cdc7fcafe05d5e7ceacd611d9d8bdd22f5f8d
Soden <f.navarronicolas@gmail.com>
2019-01-24 15:13:25 +0100

    UpDate VehicleBuilder

--- CarManagement/Builders/VehicleBuilder.cs
+++ CarManagement/Builders/VehicleBuilder.cs
@@ -5,20 +5,20 @@ namespace CarManagement.Builders
 {
     public class VehicleBuilder
     {
-        private int nWheel = 0;
-        private int doors = 0;
+        private int numberWheel = 0;
+        private int numberDoor = 0;
         private int engine = 0;
         private CarColor color;
 
 
         public void addWheel()
         {
-            this.nWheel++;
+            this.numberWheel++;
         }
 
         public void setDoors(int doorsCount)
         {
-            this.doors = ;
+            this.numberDoor = doorsCount;
         }
 
         public void setEngine(int horsePorwer)
@@ -33,8 +33,24 @@ namespace CarManagement.Builders
 
         public Vehicle build()
         {
-            return new Vehicle(nWheel, doors, engine,);
-            throw new NotImplementedException();
+            Door[] doors= new Door[this.numberDoor];
+            for (int x=0; x<this.numberDoor;x++)
+            {
+                doors[x] = new Door(x);
+            }
+
+
+            Engine engine = new Engine();
+
+            Wheel[] wheels = new Wheel[numberWheel];
+            for (int x=0; x<this.numberWheel;x++)
+            {
+                wheels[x] = new Wheel();
+            }
+
+            String enrollment = Math.Ceiling((decimal)DateTime.Now.Month).ToString();
+
+            return new Vehicle(this.color,doors,engine,wheels, enrollment);
         }
     }
 }
