3e440698e0b4710c5cbaf923170081ce168dbcae
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2019-01-24 09:43:16 +0100

    Add Engine IsStarted readonly property

--- CarManagement/Models/Engine.cs
+++ CarManagement/Models/Engine.cs
@@ -1,6 +1,16 @@
-﻿namespace CarManagement.Models
+﻿using System;
+
+namespace CarManagement.Models
 {
     public class Engine
     {
+        public bool IsStarted
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
     }
 }
