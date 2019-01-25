4443854c731383ce00da66252871e1702d1b724a
amunozgarcia <alexmg.alt@gmail.com>
2019-01-25 13:01:41 +0100

    Mod enrollmentProvider generation

--- CarManagement/Services/DefaultEnrollmentProvider.cs
+++ CarManagement/Services/DefaultEnrollmentProvider.cs
@@ -1,10 +1,36 @@
-﻿namespace CarManagement.Services
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarManagement.Services
 {
     public class DefaultEnrollmentProvider : IEnrollmentProvider
     {
+        //Random enrollmentGenerator = new Random();
+        //private const int ENROLLMENT_CHAR_QUANTITY = 8;
+        //private List<String> usedEnrollments = new List<String>();
+
+        private double enrollmentCount = 0;
+
         string IEnrollmentProvider.getNewEnrollment()
         {
-            throw new System.NotImplementedException();
+
+            String enrollmentPlate = (this.enrollmentCount++).ToString();
+
+            /*do
+            {
+                enrollmentPlate = "";
+
+                for (int enrollmentChars = 0; enrollmentChars < ENROLLMENT_CHAR_QUANTITY; enrollmentChars++)
+                {
+
+                    enrollmentPlate += enrollmentGenerator.Next();
+                }
+
+            } while (usedEnrollments.Contains(enrollmentPlate));
+
+            usedEnrollments.Add(enrollmentPlate);*/
+
+            return enrollmentPlate;
         }
     }
 }
