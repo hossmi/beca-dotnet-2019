4c0dcac73d1b43c24a902bd846ef5c259b323394
Celiamg <celiamargil@gmail.com>
2019-01-25 11:13:44 +0100

    Implementar clase DefaultEnrollmentProvider

--- CarManagement/Services/DefaultEnrollmentProvider.cs
+++ CarManagement/Services/DefaultEnrollmentProvider.cs
@@ -1,10 +1,29 @@
-﻿namespace CarManagement.Services
+﻿using System;
+
+namespace CarManagement.Services
 {
     public class DefaultEnrollmentProvider : IEnrollmentProvider
     {
+        private int numberEnrollment = 0;
+        private readonly Random generate;
+
+        public DefaultEnrollmentProvider()
+        {
+            generate = new Random();
+        }
+
         string IEnrollmentProvider.getNewEnrollment()
         {
-            throw new System.NotImplementedException();
+            string result = "";
+            result = result + (char)this.generate.Next((int)'A', (int)'Z');
+            result = result + (char)this.generate.Next((int)'A', (int)'Z');
+            result = result + (char)this.generate.Next((int)'A', (int)'Z');
+            result = result + (char)this.generate.Next((int)'0', (int)'9');
+            result = result + (char)this.generate.Next((int)'0', (int)'9');
+            result = result + (char)this.generate.Next((int)'0', (int)'9');
+            result = result + (char)this.numberEnrollment++;
+
+            return result;
         }
     }
 }
