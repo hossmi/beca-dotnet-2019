ac00edda03d43055d4a0101d58ff48bdb17021e8
Celiamg <celiamargil@gmail.com>
2019-01-28 17:00:43 +0100

    Merge to develop

--- CarManagement/Services/InMemoryVehicleStorage.cs
+++ CarManagement/Services/InMemoryVehicleStorage.cs
@@ -6,7 +6,6 @@ namespace CarManagement.Services
 {
     public class InMemoryVehicleStorage : IVehicleStorage
     {
-        //private List<Vehicle> vehicles = new List<Vehicle>();
         IDictionary<IEnrollment, Vehicle> vehicles = new Dictionary<IEnrollment, Vehicle>();
 
         public int Count {
@@ -24,16 +23,16 @@ namespace CarManagement.Services
         public Vehicle get(IEnrollment enrollment)
         {
             Vehicle vehicle;
-            bool exists = vehicles.TryGetValue(defaultEnrollment, out vehicle);
+            bool exists = vehicles.TryGetValue(enrollment, out vehicle);
             Asserts.isTrue(exists);
             return vehicle;
         }
 
         public void set(Vehicle vehicle)
         {
-            bool exists = vehicles.ContainsKey(motoVehicle.Enrollment);
+            bool exists = vehicles.ContainsKey(vehicle.Enrollment);
             Asserts.isFalse(exists);
-            this.vehicles.Add(motoVehicle.Enrollment, motoVehicle);
+            this.vehicles.Add(vehicle.Enrollment, vehicle);
         }
     }
 }
