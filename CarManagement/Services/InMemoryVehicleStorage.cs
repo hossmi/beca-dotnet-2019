aac53b09ac9d08cd15c5c80fe6671664daec4dfd
evoblackshark <kilowan2@gmail.com>
2019-01-29 11:06:04 +0100

    Update InMemoryVehicleStorage

--- CarManagement/Services/InMemoryVehicleStorage.cs
+++ CarManagement/Services/InMemoryVehicleStorage.cs
@@ -1,24 +1,43 @@
 ﻿using CarManagement.Models;
+using System.Collections.Generic;
 
 namespace CarManagement.Services
 {
     public class InMemoryVehicleStorage : IVehicleStorage
     {
-        public int Count { get; }
+
+        private List<Vehicle> list_vehicle = new List<Vehicle>();
+
+        public int Count {
+            get
+            {
+                return list_vehicle.Count;
+            }
+        }
 
         public void clear()
         {
-            throw new System.NotImplementedException();
+            list_vehicle.Clear();
         }
 
         public Vehicle get(IEnrollment enrollment)
         {
-            throw new System.NotImplementedException();
+            Vehicle T2 = new Vehicle();
+            foreach (Vehicle T in list_vehicle)
+            {
+                Asserts.isTrue(enrollment == T.Enrollment);
+                T2 = T;
+                /*if (enrollment == T.Enrollment)
+                {
+                    T2 = T;
+                }*/
+            }
+            return T2;
         }
 
         public void set(Vehicle vehicle)
         {
-            throw new System.NotImplementedException();
+            list_vehicle.Add(vehicle);
         }
     }
 }
