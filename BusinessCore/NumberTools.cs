2d15a669c476d40db2e50391e617e54a68379e14
Cures9 <jorgeluis92cc@gmail.com>
2019-01-29 15:12:36 +0100

    implement fibonacci and factorials methods

--- BusinessCore/NumberTools.cs
+++ BusinessCore/NumberTools.cs
@@ -10,12 +10,26 @@ namespace BusinessCore
     {
         public static long factorial(long n)
         {
-            throw new NotImplementedException();
+            long resultado = 1;
+
+            for (long i = 1; i <= n; i++)
+            {
+               resultado = resultado * i;
+            }
+            return resultado;
         }
 
         public static int fibonacci(int n)
         {
-            throw new NotImplementedException();
+            int num1 = 0;
+            int num2 = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = num1;
+                num1 = num2;
+                num2 = temp + num2;
+            }
+            return num1;
         }
-    }
+}
 }
