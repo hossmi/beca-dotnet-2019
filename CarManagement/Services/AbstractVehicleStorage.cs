8ba59dbaac31fc68e3672d966e502fd32d0b5805
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2019-01-29 14:48:20 +0100

    half abrstract class implementation

--- CarManagement/Services/AbstractVehicleStorage.cs
+++ CarManagement/Services/AbstractVehicleStorage.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CarManagement.Models;
 
 namespace CarManagement.Services
 {
-    public abstract class AbstractVehicleStorage
+    public abstract class AbstractVehicleStorage : IVehicleStorage
     {
+        private readonly IDictionary<IEnrollment, Vehicle> vehicles;
+
+        public AbstractVehicleStorage()
+        {
+
+        }
+
+        public int Count { get; }
+
+        public void clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vehicle get(IEnrollment enrollment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void set(Vehicle vehicle)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
