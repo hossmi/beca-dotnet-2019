a6b430a8153206edf4ef97e96a3ca193bebb4f5d
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2019-01-29 10:39:33 +0100

    add IDtoConverter interface and its default implementation class

---
+++ CarManagement/Services/IDtoConverter.cs
@@ -0,0 +1,19 @@
+﻿using CarManagement.Models;
+using CarManagement.Models.DTOs;
+
+namespace CarManagement.Services
+{
+    public interface IDtoConverter
+    {
+        Engine convert(EngineDto engineDto);
+        EngineDto convert(Engine engine);
+        Vehicle convert(VehicleDto vehicleDto);
+        VehicleDto convert(Vehicle vehicle);
+        Door convert(DoorDto doorDto);
+        DoorDto convert(Door door);
+        Wheel convert(WheelDto wheelDto);
+        WheelDto convert(Wheel wheel);
+        IEnrollment convert(EnrollmentDto enrollmentDto);
+        EnrollmentDto convert(IEnrollment enrollment, IEnrollmentProvider enrollmentProvider);
+    }
+}
