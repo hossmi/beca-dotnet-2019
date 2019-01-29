a0c6c4fac098253614714765716e23332f5ac718
Celiamg <celiamargil@gmail.com>
2019-01-29 09:55:27 +0100

    Create class MapsDto

---
+++ CarManagement/Models/DTOs/MapsDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManagement.Models.DTOs
+{
+    class MapsDto
+    {
+        public EngineDto convert(Engine engine)
+        {
+            EngineDto engineDto = new EngineDto();
+            engineDto.HorsePower = engine.HorsePorwer;
+            engineDto.IsStarted = engine.IsStarted;
+
+            return engineDto;
+        }
+
+        public Engine convert(EngineDto engineDto)
+        {
+            Engine engine = new Engine(engineDto.HorsePower);
+            engineDto.IsStarted = engine.IsStarted;
+
+            return engine;
+        }
+
+        public EnrollmentDto convert()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
