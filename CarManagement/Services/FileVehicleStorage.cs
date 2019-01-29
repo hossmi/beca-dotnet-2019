cad905f72fc8989104ac4221bacaf1ccd9bd8d61
Necoleto <xyztresd@gmail.com>
2019-01-29 13:30:16 +0100

    Update serializablemethod

--- CarManagement/Services/FileVehicleStorage.cs
+++ CarManagement/Services/FileVehicleStorage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Xml.Serialization;
 using CarManagement.Models;
 
 namespace CarManagement.Services
@@ -14,15 +15,16 @@ namespace CarManagement.Services
         public FileVehicleStorage(string fileFullPath, IDtoConverter dtoConverter)
         {
             this.filePath = fileFullPath;
-            this.vehicles = readFromFile(fileFullPath);
+            this.vehicles = readFromFile(fileFullPath); //Lee y si exite deserealiza y lo mete al dicccionario(en memoria)
             this.dtoConverter = dtoConverter;
         }
 
         public int Count { get; }
 
         public void clear()
+
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException(); 
             writeToFile(this.filePath, this.vehicles);
         }
 
@@ -40,12 +42,41 @@ namespace CarManagement.Services
         private static void writeToFile(string filePath, IDictionary<IEnrollment,Vehicle> vehicles)
         {
             //https://docs.microsoft.com/es-es/dotnet/standard/serialization/examples-of-xml-serialization
-            throw new NotImplementedException();
+
+            Vehicle[] listVehicles = new Vehicle[vehicles.Count];
+            int aux = 0;
+            foreach(Vehicle v  in vehicles.Values)
+            {
+                listVehicles[aux] = v;
+                aux++;
+            }
+            XmlSerializer ser = new XmlSerializer(typeof (Vehicle[]));
+            TextWriter writer = new StreamWriter(filePath);
+            ser.Serialize(writer, listVehicles);
+            writer.Close();
+
         }
 
         private static IDictionary<IEnrollment, Vehicle> readFromFile(string fileFullPath)
+
+
         {
-            throw new NotImplementedException();
+            IDictionary<IEnrollment, Vehicle> vehicles = new Dictionary<IEnrollment, Vehicle>();
+            // te pasan una ruta y 
+            if (File.Exists(fileFullPath) == true)
+            {
+                XmlSerializer ser = new XmlSerializer(typeof(Vehicle[]));
+                TextReader reader = new StreamReader(fileFullPath );
+                Vehicle[] listVehicle=(Vehicle[])ser.Deserialize(reader); //guardo lo que he leido en un array de vehiculos.
+                reader.Close();
+                
+                foreach (Vehicle v in listVehicle )
+                {
+                    vehicles.Add(v.Enrollment, v);
+                }
+            }
+            return vehicles;
+                
         }
 
     }
