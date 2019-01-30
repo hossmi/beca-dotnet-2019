65e2533257edcbd60a205047c6df30bd96a66fc0
jvbb <jvbb@hotmail.es>
2019-01-30 16:31:49 +0100

    Update before Merge

--- BusinessCore.Tests/JVBBTests.cs
+++ BusinessCore.Tests/JVBBTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using CarManagement.Builders;
 using CarManagement.Models;
+using CarManagement.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BusinessCore.Tests
@@ -9,6 +12,22 @@ namespace BusinessCore.Tests
     [TestCategory("JVBB Tests")]
     public class JVBBTests
     {
+        public string VehiclesFilePath
+        {
+            get
+            {
+                return Path.Combine(Environment.CurrentDirectory, "Vehicles.xml");
+            }
+        }
+
+        [TestInitialize]
+        [TestCleanup]
+        public void initialize()
+        {
+            if (File.Exists(this.VehiclesFilePath))
+                File.Delete(this.VehiclesFilePath);
+        }
+        
         [TestMethod]
         public void WrongBuildRequests_01()
         {
@@ -42,6 +61,43 @@ namespace BusinessCore.Tests
 
         }
 
+        [TestMethod]
+        public void StorageComparison()
+        {
+            Vehicle motoVehicle;
+
+            FakeEnrollmentProvider enrollmentProvider = new FakeEnrollmentProvider();
+            IEqualityComparer<IEnrollment> equalityComparer = new EnrollmentEqualityComparer();
+            IVehicleBuilder vehicleBuilder = new VehicleBuilder(enrollmentProvider);
+            IDtoConverter dtoConverter = new DefaultDtoConverter(enrollmentProvider);
+            IVehicleStorage vehicleFileStorage = new FileVehicleStorage(this.VehiclesFilePath, dtoConverter);
+            IVehicleStorage vehicleMemoryStorageA = new InMemoryVehicleStorage();
+            IVehicleStorage vehicleMemoryStorageB = new InMemoryVehicleStorage();
+
+            vehicleBuilder.addWheel();
+            vehicleBuilder.addWheel();
+            vehicleBuilder.setDoors(0);
+            vehicleBuilder.setEngine(40);
+            motoVehicle = vehicleBuilder.build();
+
+            vehicleFileStorage.set(motoVehicle);
+            vehicleMemoryStorageA.set(motoVehicle);
+            vehicleMemoryStorageB.set(motoVehicle);
+            vehicleFileStorage = new FileVehicleStorage(this.VehiclesFilePath, dtoConverter);
+
+            Vehicle memoryVehicleA = vehicleMemoryStorageA.get(enrollmentProvider.DefaultEnrollment);
+            Vehicle memoryVehicleB = vehicleMemoryStorageB.get(enrollmentProvider.DefaultEnrollment);
+            Vehicle fileVehicle = vehicleFileStorage.get(enrollmentProvider.DefaultEnrollment);
+
+            Assert.IsNotNull(memoryVehicleA);
+            Assert.IsNotNull(memoryVehicleB);
+            Assert.IsNotNull(vehicleFileStorage);
+
+            Assert.AreEqual(memoryVehicleA, memoryVehicleB);
+            Assert.AreEqual(memoryVehicleA, fileVehicle);
+            Assert.AreEqual(memoryVehicleB, fileVehicle);
+
+        }
 
     }
 }
--- CarManagement/Services/InMemoryVehicleStorage.cs
+++ CarManagement/Services/InMemoryVehicleStorage.cs
@@ -7,47 +7,9 @@ namespace CarManagement.Services
     public class InMemoryVehicleStorage : AbstractVehicleStorage
     {
         public InMemoryVehicleStorage() : base(new Dictionary<IEnrollment, Vehicle>())
-        {
-        }
-
-        //private IDictionary<IEnrollment, Vehicle> vehicles = new Dictionary<IEnrollment, Vehicle>();
-
-        //public int Count
-        //{
-        //    get
-        //    {
-        //        return this.vehicles.Count;
-        //    }
-        //}
-
-        //public void clear()
-        //{
-        //    this.vehicles.Clear();
-        //}
-
-        //public Vehicle get(IEnrollment enrollment)
-        //{
-        //    //Asserts.isTrue(vehicles.Count > 0,"La lista de vehiculos esta vacia");
-        //    Vehicle vehicle;
-        //    bool vehicleFound = this.vehicles.TryGetValue(enrollment, out vehicle);
-        //    Asserts.isTrue(vehicleFound,"Cannot find vehicle");
-        //    return vehicle;
-        //}
-
-        //public void set(Vehicle vehicle)
-        //{
-        //    Asserts.isFalse(this.vehicles.ContainsKey(vehicle.Enrollment), "A vehicle already exists with the same enrollment");
-        //    this.vehicles.Add(vehicle.Enrollment, vehicle);
-        //}
-
-        //protected override IDictionary<IEnrollment, Vehicle> load()
-        //{
-        //    return new Dictionary<IEnrollment, Vehicle>();
-        //}
+        { }
 
         protected override void save(IEnumerable<Vehicle> vehicles)
-        {
-            //this is already implemented in the parent class.
-        }
+        { }
     }
 }
