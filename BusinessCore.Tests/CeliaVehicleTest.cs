f80c46e241d75edf7f5126e6cc120b2ad252f7ce
Celiamg <celiamargil@gmail.com>
2019-01-30 13:30:05 +0100

    Include CeliaVehicleTest

---
+++ BusinessCore.Tests/CeliaVehicleTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CarManagement.Builders;
+using CarManagement.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessCore.Tests
+{
+    [TestClass]
+    public class CeliaVehicleTest
+    {
+        [TestMethod]
+        public void cannot_removeWheell_if_there_is_no()
+        {
+            IEnrollmentProvider enrollmentProvider = new DefaultEnrollmentProvider();
+            IVehicleBuilder builder = new VehicleBuilder(enrollmentProvider);
+
+            builder.addWheel();
+            builder.addWheel();
+            builder.removeWheel();
+            builder.removeWheel();
+
+            Negassert.mustFail(() => builder.removeWheel());
+        }
+    }
+}
