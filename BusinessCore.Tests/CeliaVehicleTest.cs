0bf16e84a17ee721e678ab8a7ad958e2207a7198
Celia Martinez <celiamarg@hotmail.es>
2019-01-31 16:16:37 +0100

    Create tests

--- BusinessCore.Tests/CeliaVehicleTest.cs
+++ BusinessCore.Tests/CeliaVehicleTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CarManagement.Builders;
+using CarManagement.Models;
 using CarManagement.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -20,6 +21,35 @@ namespace BusinessCore.Tests
             builder.removeWheel();
 
             Negassert.mustFail(() => builder.removeWheel());
+        }
+
+        [TestMethod]
+        public void cannot_setDoors_15()
+        {
+            IEnrollmentProvider enrollmentProvider = new DefaultEnrollmentProvider();
+            IVehicleBuilder builder = new VehicleBuilder(enrollmentProvider);
+
+            Negassert.mustFail(() => builder.setDoors(15));
+        }
+
+        [TestMethod]
+        public void cannot_openDoor_twice()
+        {
+            FakeEnrollmentProvider enrollmentProvider = new FakeEnrollmentProvider();
+            IVehicleBuilder builder = new VehicleBuilder(enrollmentProvider);
+
+            builder.addWheel();
+            builder.addWheel();
+            builder.addWheel();
+            builder.addWheel();
+
+            builder.setDoors(2);
+            builder.setEngine(100);
+            builder.setColor(CarColor.Red);
+
+            Vehicle vehicle = builder.build();
+            vehicle.Doors[0].open();
+            Negassert.mustFail(() => vehicle.Doors[0].open());
         }
     }
 }
