df003600de2c9f9696ecdd46d506259f93eb2fc2
evoblackshark <kilowan2@gmail.com>
2019-02-01 11:26:54 +0100

    Update DDC

--- CarManagement/Services/DefaultDtoConverter.cs
+++ CarManagement/Services/DefaultDtoConverter.cs
@@ -7,59 +7,84 @@ namespace CarManagement.Services
     {
         private IEnrollmentProvider enrollmentProvider;
 
+        //Memoria ->Fichero
         public DefaultDtoConverter(IEnrollmentProvider enrollmentProvider)
         {
             this.enrollmentProvider = enrollmentProvider;
         }
 
+        //Fichero ->Memoria
         public Engine convert(EngineDto engineDto)
         {
             throw new System.NotImplementedException();
         }
 
+        //Memoria ->Fichero
         public EngineDto convert(Engine engine)
         {
-            throw new System.NotImplementedException();
+            EngineDto engineDto = new EngineDto();
+            engineDto.HorsePower = engine.Horsepower;
+            engineDto.IsStarted = engine.IsStarted;
+
+            return engineDto;
         }
 
+        //Fichero ->Memoria
         public Vehicle convert(VehicleDto vehicleDto)
         {
             throw new System.NotImplementedException();
         }
 
+        //Memoria ->Fichero
         public VehicleDto convert(Vehicle vehicle)
         {
-            throw new System.NotImplementedException();
+            VehicleDto vehicleDto = new VehicleDto();
+            vehicleDto.Color = vehicle.carcolor;
+            vehicleDto.Doors = vehicle.Doors;
+            return vehicleDto;
         }
 
+        //Fichero ->Memoria
         public Door convert(DoorDto doorDto)
         {
             throw new System.NotImplementedException();
         }
 
+        //Memoria ->Fichero
         public DoorDto convert(Door door)
         {
-            throw new System.NotImplementedException();
+            DoorDto doorDto = new DoorDto();
+            doorDto.IsOpen = door.IsOpen;
+            return doorDto;
         }
 
+        //Fichero ->Memoria
         public Wheel convert(WheelDto wheelDto)
         {
             throw new System.NotImplementedException();
         }
 
+        //Memoria ->Fichero
         public WheelDto convert(Wheel wheel)
         {
-            throw new System.NotImplementedException();
+            WheelDto wheelDto = new WheelDto();
+            wheelDto.Pressure = wheel.Pressure;
+            return wheelDto;
         }
 
+        //Fichero ->Memoria
         public IEnrollment convert(EnrollmentDto enrollmentDto)
         {
             throw new System.NotImplementedException();
         }
 
+        //Memoria ->Fichero
         public EnrollmentDto convert(IEnrollment enrollment)
         {
-            throw new System.NotImplementedException();
+            EnrollmentDto enrollmentDto = new EnrollmentDto();
+            enrollmentDto.Number = enrollment.Number;
+            enrollmentDto.Serial = enrollment.Serial;
+            return enrollmentDto;
         }
     }
 }
