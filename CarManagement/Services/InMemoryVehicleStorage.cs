c475735558a76cd48c60d511590f6af0eec8ef8c
Necoleto <xyztresd@gmail.com>
2019-02-01 12:53:42 +0100

    Upgrade VehicleStorage

--- CarManagement/Services/InMemoryVehicleStorage.cs
+++ CarManagement/Services/InMemoryVehicleStorage.cs
@@ -9,25 +9,13 @@ namespace CarManagement.Services
 {
     public class InMemoryVehicleStorage : AbstractVehicleStorage
     {
-        IDictionary<IEnrollment, Vehicle> vehicles;
-
-          public InMemoryVehicleStorage() : base(load())
+             public InMemoryVehicleStorage() : base(new Dictionary<IEnrollment,Vehicle>())
         {
-           this.vehicles.Clear();
+          
         }
-
         protected override void save(IEnumerable<Vehicle> vehicles)
         {
 
-            load(this.vehicles );
-
-        }
-
-        private static IDictionary<IEnrollment, Vehicle> load()
-        {
-            Asserts.isFalse(this.vehicles.ContainsKey(vehicle.Enrollment));
-            this.vehicles.Add(vehicle.Enrollment,vehicle);
-            return new Dictionary<IEnrollment, Vehicle>(new EnrollmentEqualityComparer()); //linea añadida
         }
     }
 }
