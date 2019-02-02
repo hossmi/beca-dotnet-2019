530eb7e2be03fe174c15587f7a61f6a65d464f4d
Necoleto <46891882+Necoleto@users.noreply.github.com>
2019-02-02 18:36:30 +0100

    Upgrade IDtoConverter

--- CarManagement/Services/IDtoConverter.cs
+++ CarManagement/Services/IDtoConverter.cs
@@ -1,6 +1,19 @@
-﻿namespace CarManagement.Services
+﻿using CarManagement.Core.Models;
+using CarManagement.Core.Models.DTOs;
+
+namespace CarManagement.Services
 {
     internal interface IDtoConverter
     {
+        IEngine convert(EngineDto engineDto);
+        EngineDto convert(IEngine engine);
+        IVehicle convert(VehicleDto vehicleDto);
+        VehicleDto convert(IVehicle vehicle);
+        IDoor convert(DoorDto doorDto);
+        DoorDto convert(IDoor door);
+        IWheel convert(WheelDto wheelDto);
+        WheelDto convert(IWheel wheel);
+        IEnrollment convert(EnrollmentDto enrollmentDto);
+        EnrollmentDto convert(IEnrollment enrollment);
     }
 }
