f952b91de2d3825a44e715427b9827cc81182b42
Soden <f.navarronicolas@gmail.com>
2019-02-02 17:08:08 +0100

    NumberTools tests OK

--- BusinessCore/NumberTools.cs
+++ BusinessCore/NumberTools.cs
@@ -10,12 +10,26 @@ namespace BusinessCore
     {
         public static long factorial(long n)
         {
-            throw new NotImplementedException();
+            int i;
+            long f = 1;
+
+            for (i = 1; i <= n; i++)
+                f = f * i;
+            return f;
         }
 
         public static int fibonacci(int n)
         {
-            throw new NotImplementedException();
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
         }
     }
 }
