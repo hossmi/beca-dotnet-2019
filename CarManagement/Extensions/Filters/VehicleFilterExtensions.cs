4eee263f2e73aaed1be834a966b912e109c29ab4
alloret1993 <alloret1993@outlook.com>
2019-02-04 10:58:45 +0100

    pre merge

--- CarManagement/Extensions/Filters/VehicleFilterExtensions.cs
+++ CarManagement/Extensions/Filters/VehicleFilterExtensions.cs
@@ -9,15 +9,30 @@ namespace CarManagement.Extensions.Filters
         public static IEnumerable<IVehicle> getVehiclesByPairEnrollments(
             this IEnumerable<IVehicle> vehicles)
         {
-            List<IVehicle> pairVehicles = new List<IVehicle>;
+            List<IVehicle> pairVehicles = new List<IVehicle>();
+            { 
+            foreach (IVehicle vehicle in vehicles)
+                if (vehicle.Enrollment.Number % 2 == 0) //repasar bien los % (resto)
+                    pairVehicles.Add(vehicle);
+            }
 
-      
+            return pairVehicles;
         }
+
+
 
         public static IEnumerable<IVehicle> getVehiclesByEnrollmentsSerial(
             this IEnumerable<IVehicle> vehicles, string enrollmentSerial)
         {
-            throw new NotImplementedException();
+            List<IVehicle> serialVehicles = new List<IVehicle>();
+
+            foreach (IVehicle vehicle in vehicles)
+            {
+                if (vehicle.Enrollment.Serial.ToString() == enrollmentSerial)
+                    serialVehicles.Add(vehicle);
+            }
+
+            return serialVehicles;
         }
     }
 }
