c6b9b6a11adb9726bfb8df338228f93cdda30605
Necoleto <xyztresd@gmail.com>
2019-02-04 11:56:37 +0100

    Upgrade getVehiclesByPairEnrollments method

--- CarManagement/Extensions/Filters/VehicleFilterExtensions.cs
+++ CarManagement/Extensions/Filters/VehicleFilterExtensions.cs
@@ -9,13 +9,23 @@ namespace CarManagement.Extensions.Filters
         public static IEnumerable<IVehicle> getVehiclesByPairEnrollments(
             this IEnumerable<IVehicle> vehicles)
         {
-            throw new NotImplementedException();
+
+            var CurrentEnrollment = vehicles.GetEnumerator();
+            foreach (IVehicle vehiclesPairNumber in vehicles)
+            {
+                if (CurrentEnrollment.Current.Enrollment.Number % 2 == 0)
+                {
+                    yield return vehiclesPairNumber;
+                }
+            }
+            
         }
 
         public static IEnumerable<IVehicle> getVehiclesByEnrollmentsSerial(
             this IEnumerable<IVehicle> vehicles, string enrollmentSerial)
         {
             throw new NotImplementedException();
+
         }
 
         public static IEnumerable<IEngine> getEngines(this IEnumerable<IVehicle> vehicles)
