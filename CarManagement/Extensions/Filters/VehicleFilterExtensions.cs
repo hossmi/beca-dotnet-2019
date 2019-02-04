3367d21fb9c5b29ba9b7618e4658d9b984127ae3
soden <sodenplayer@gmail.com>
2019-02-04 13:04:42 +0100

    Add clear using VehicleFilterExtensions

--- CarManagement/Extensions/Filters/VehicleFilterExtensions.cs
+++ CarManagement/Extensions/Filters/VehicleFilterExtensions.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using CarManagement.Core.Models;
-using System.Linq;
 
 namespace CarManagement.Extensions.Filters
 {
