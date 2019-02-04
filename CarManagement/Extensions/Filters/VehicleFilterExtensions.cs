3830b98e36a9554432533cd22486a943228595c9
jvbb <jvbb@hotmail.es>
2019-02-04 15:24:05 +0100

    Update Filters

--- CarManagement/Extensions/Filters/VehicleFilterExtensions.cs
+++ CarManagement/Extensions/Filters/VehicleFilterExtensions.cs
@@ -7,57 +7,77 @@ namespace CarManagement.Extensions.Filters
 {
     public static class VehicleFilterExtensions
     {
-        public static IEnumerable<IVehicle> filterByPairEnrollments(
-            this IEnumerable<IVehicle> vehicles)
-        {
-            foreach (IVehicle vehicle in vehicles)
-            {
-                if (vehicle.Enrollment.Number % 2 == 0)
-                    yield return vehicle;
-            }
-        }
+        //public static IEnumerable<IVehicle> filterByPairEnrollments(
+        //    this IEnumerable<IVehicle> vehicles)
+        //{
+        //    foreach (IVehicle vehicle in vehicles)
+        //    {
+        //        if (vehicle.Enrollment.Number % 2 == 0)
+        //            yield return vehicle;
+        //    }
+        //}
 
-        public static IEnumerable<IVehicle> filterByEnrollmentsSerial(
-            this IEnumerable<IVehicle> vehicles, string enrollmentSerial)
-        {
-            IEnumerator<IVehicle> enumVehicle = vehicles.GetEnumerator();
+        //public static IEnumerable<IVehicle> filterByEnrollmentsSerial(
+        //    this IEnumerable<IVehicle> vehicles, string enrollmentSerial)
+        //{
+        //    IEnumerator<IVehicle> enumVehicle = vehicles.GetEnumerator();
 
-            while (enumVehicle.MoveNext())
-            {
-                if (enumVehicle.Current.Enrollment.Serial == enrollmentSerial)
-                    yield return enumVehicle.Current;
-            }
-        }
+        //    while (enumVehicle.MoveNext())
+        //    {
+        //        if (enumVehicle.Current.Enrollment.Serial == enrollmentSerial)
+        //            yield return enumVehicle.Current;
+        //    }
+        //}
 
         public static IEnumerable<IEngine> selectEngines(this IEnumerable<IVehicle> vehicles)
         {
             foreach (IVehicle vehicle in vehicles)
             {
-                    yield return vehicle.Engine;
+                yield return vehicle.Engine;
             }
         }
 
-        public static IEnumerable<IEngine> filterByStarted(this IEnumerable<IEngine> engines)
+        //public static IEnumerable<IEngine> filterByStarted(this IEnumerable<IEngine> engines)
+        //{
+        //    foreach (IEngine engine in engines)
+        //    {
+        //        if (engine.IsStarted)
+        //            yield return engine;
+        //    }
+        //}
+
+        //public static IEnumerable<IEngine> filterByHorsePowerGreaterOrEqual(this IEnumerable<IEngine> engines, int horsePower)
+        //{
+        //    foreach (IEngine engine in engines)
+        //    {
+        //        if (engine.HorsePower >= horsePower)
+        //            yield return engine;
+        //    }
+        //}
+
+        public static bool filterByPairEnrollments(IVehicle vehicle)
         {
-            foreach (IEngine engine in engines)
-            {
-                if (engine.IsStarted)
-                    yield return engine;
-            }
+            return vehicle.Enrollment.Number % 2 == 0;
         }
 
-        public static IEnumerable<IEngine> filterByHorsePowerGreaterOrEqual(this IEnumerable<IEngine> engines, int horsePower)
+        public static bool filterByEnrollmentsSerial(IVehicle vehicle, string enrollmentSerial)
         {
-            foreach (IEngine engine in engines)
-            {
-                if (engine.HorsePower >= horsePower)
-                    yield return engine;
-            }
+            return vehicle.Enrollment.Serial == enrollmentSerial;
+        }
+
+        public static IEngine selectEngine(IVehicle vehicle)
+        {
+            return vehicle.Engine;
         }
 
         public static bool filterByIsStarted(IEngine engine)
         {
             return engine.IsStarted;
+        }
+
+        public static bool filterByHorsePowerGreaterOrEqual(IEngine engine, int horsePower)
+        {
+            return engine.HorsePower >= horsePower;
         }
 
         public static IEnumerable<T> filter<T>(
@@ -69,5 +89,16 @@ namespace CarManagement.Extensions.Filters
                     yield return item;
             }
         }
+
+        public static IEnumerable<T> filter<T,T2>(
+            this IEnumerable<T> items, Func<T,T2, bool> filterDelegate,T2 value)
+        {
+            foreach (T item in items)
+            {
+                if (filterDelegate(item,value))
+                    yield return item;
+            }
+        }
+
     }
 }
