6e33a0f4c1a98714e4c5aaba41ab31560b3622fc
EpicSimple <anton_the@hotmail.es>
2019-02-04 08:31:07 +0100

    test working, change applied

--- CarManagement/Services/VehicleBuilder.cs
+++ CarManagement/Services/VehicleBuilder.cs
@@ -122,12 +122,98 @@ namespace CarManagement.Services
 
         public IVehicle import(VehicleDto vehicleDto)
         {
-            throw new NotImplementedException();
+            List<IDoor> doors = new List<IDoor>();
+            foreach (DoorDto door in vehicleDto.Doors)
+            {
+                doors.Add(this.convert(door));
+            }
+
+            List<IWheel> wheels = new List<IWheel>();
+            foreach (WheelDto wheel in vehicleDto.Wheels)
+            {
+                wheels.Add(this.convert(wheel));
+            }
+
+            return new Vehicle(this.convert(vehicleDto.Engine), doors, wheels, vehicleDto.Color, this.convert(vehicleDto.Enrollment));
         }
 
-        public VehicleDto export(IVehicle vehicleDto)
+        public VehicleDto export(IVehicle vehicle)
         {
-            throw new NotImplementedException();
+            VehicleDto vehicleDto = new VehicleDto
+            {
+                Color = vehicle.Color,
+                Engine = this.convert(vehicle.Engine),
+                Enrollment = this.convert(vehicle.Enrollment)
+            };
+            List<DoorDto> dtoDoorsList = new List<DoorDto>();
+            foreach (Door door in vehicle.Doors)
+            {
+                dtoDoorsList.Add(this.convert(door));
+            }
+            vehicleDto.Doors = dtoDoorsList.ToArray();
+
+            List<WheelDto> dtoWheelsList = new List<WheelDto>();
+            foreach (Wheel wheel in vehicle.Wheels)
+            {
+                dtoWheelsList.Add(this.convert(wheel));
+            }
+            vehicleDto.Wheels = dtoWheelsList.ToArray();
+
+            return vehicleDto;
+        }
+
+        private IEngine convert(EngineDto engineDto)
+        {
+            return new Engine(engineDto.HorsePower, engineDto.IsStarted);
+        }
+
+        private EngineDto convert(IEngine engine)
+        {
+            return new EngineDto
+            {
+                HorsePower = engine.HorsePower,
+                IsStarted = engine.IsStarted
+            };
+        }
+
+        private IDoor convert(DoorDto doorDto)
+        {
+            return new Door(doorDto.IsOpen);
+        }
+
+        private DoorDto convert(IDoor door)
+        {
+            return new DoorDto
+            {
+                IsOpen = door.IsOpen,
+            };
+        }
+
+        private IWheel convert(WheelDto wheelDto)
+        {
+            return new Wheel(wheelDto.Pressure);
+        }
+
+        private WheelDto convert(IWheel wheel)
+        {
+            return new WheelDto
+            {
+                Pressure = wheel.Pressure,
+            };
+        }
+
+        private IEnrollment convert(EnrollmentDto enrollmentDto)
+        {
+            return this.enrollmentProvider.import(enrollmentDto.Serial, enrollmentDto.Number);
+        }
+
+        private EnrollmentDto convert(IEnrollment enrollment)
+        {
+            return new EnrollmentDto
+            {
+                Serial = enrollment.Serial,
+                Number = enrollment.Number
+            };
         }
     }
 }
