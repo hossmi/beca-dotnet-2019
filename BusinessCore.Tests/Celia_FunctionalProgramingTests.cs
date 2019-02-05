3fb82c8b8d7c70903629ae5f2f1c69eea9bf6fb6
Celiamg <celiamargil@gmail.com>
2019-02-05 15:28:33 +0100

    Add get_the_vehicles_black_with_all_their_doors_closed()

--- BusinessCore.Tests/Celia_FunctionalProgramingTests.cs
+++ BusinessCore.Tests/Celia_FunctionalProgramingTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using BusinessCore.Tests.Services;
+using CarManagement.Core.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BusinessCore.Tests
@@ -17,20 +18,18 @@ namespace BusinessCore.Tests
 
         [TestMethod]
         public void get_the_enrollment_and_horsePower_of_the_vehicles_ordered_descending_by_horsePower()
-        {
-            var vehicles = this.vehicleStorage
-                .getAll()
-                .Select(vehicle => new
-                {
-                    vehicle.Enrollment,
-                    vehicle.Engine.HorsePower
-                })
-                .OrderByDescending(vehicle => vehicle.HorsePower)
-                .ToArray();
+        {          
 
-            Assert.AreEqual(100, vehicles[0].HorsePower);
-            //Assert.AreEqual(600, vehicles[5].HorsePower);
-            //Assert.AreEqual(100, vehicles[9].HorsePower);
+            Assert.AreEqual(666, vehicles[0].HorsePower);
+            Assert.AreEqual(600, vehicles[5].HorsePower);
+            Assert.AreEqual(100, vehicles[9].HorsePower);
+        }
+
+        [TestMethod]
+        public void get_the_vehicles_black_with_all_their_doors_closed()
+        {           
+
+            Assert.AreEqual(5, vehicles.Length);
         }
     }
 }
