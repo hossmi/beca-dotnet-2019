48ccd14187c22efeb8436889d18dbef53a0b1f41
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2019-02-05 16:25:49 +0100

    add empty SqlVehicleStorageTests

---
+++ BusinessCore.Tests/SqlVehicleStorageTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessCore.Tests
+{
+    [TestClass]
+    public class SqlVehicleStorageTests
+    {
+        public SqlVehicleStorageTests()
+        {
+            
+        }
+
+    }
+}
