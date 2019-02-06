1dd0e60502f3d3d8a6053d5794c8ab27b740c0b2
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2019-02-06 11:14:38 +0100

    add sql first test

--- BusinessCore.Tests/SqlVehicleStorageTests.cs
+++ BusinessCore.Tests/SqlVehicleStorageTests.cs
@@ -1,27 +1,50 @@
 ﻿using System;
+using System.Configuration;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BusinessCore.Tests
 {
+    [TestCategory("Requires SQL Server connection")]
     [TestClass]
     public class SqlVehicleStorageTests
     {
+        private const string ConnectionStringKey = "CarManagerConnectionString";
+        private readonly string connectionString;
+
         public SqlVehicleStorageTests()
         {
-            
+            Assert.IsTrue(ConfigurationManager.AppSettings.AllKeys.Contains(ConnectionStringKey));
+            this.connectionString = ConfigurationManager.AppSettings[ConnectionStringKey].ToString();
         }
 
         [TestInitialize]
-        public void drop_and_create()
+        public void recreate()
         {
-            drop();
-
+            drop(this.connectionString);
+            create(this.connectionString);
         }
 
         [TestCleanup]
-        public void drop()
+        public void cleanUp()
         {
-
+            drop(this.connectionString);
         }
+
+        [TestMethod]
+        public void create_and_drop_works()
+        {
+        }
+
+        private static void create(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void drop(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
