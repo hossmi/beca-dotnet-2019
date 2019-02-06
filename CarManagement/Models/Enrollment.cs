68128d3bfa2f7cf2ad3e7db0824c434e2e16945b
adrianla <the_adrianin23@hotmail.com>
2019-02-06 16:52:16 +0100

    add Enrollment class

---
+++ CarManagement/Models/Enrollment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarManagement.Models
+{
+    public class Enrollment
+    {
+        private string serial;
+        private int number;
+
+        private string Serial
+        {
+            get
+            {
+                return this.serial;
+            }
+            set
+            {
+                value = this.serial;
+            }
+        }
+        
+        private int Number
+        {
+            get
+            {
+                return this.number;
+            }
+            set
+            {
+                value = this.number;
+            }
+        }
+    }
+}
