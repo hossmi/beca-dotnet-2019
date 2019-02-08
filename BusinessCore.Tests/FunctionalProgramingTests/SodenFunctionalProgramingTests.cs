882463ae83262d4dfc3b4895b1240cb7fda5779c
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2019-02-08 09:23:48 +0100

    remove method categories

--- BusinessCore.Tests/FunctionalProgramingTests/SodenFunctionalProgramingTests.cs
+++ BusinessCore.Tests/FunctionalProgramingTests/SodenFunctionalProgramingTests.cs
@@ -22,8 +22,6 @@ namespace BusinessCore.Tests
         }
 
         [TestMethod]
-        [TestCategory("Soden")]
-        [TestCategory("Functional Programing")]
         public void get_there_are_3_black_vehicles_and_horsePorwer_min100_and_started()
         {
             IVehicle[] vehicles = this.vehicleStorage
@@ -35,8 +33,6 @@ namespace BusinessCore.Tests
         }
 
         [TestMethod]
-        [TestCategory("Soden")]
-        [TestCategory("Functional Programing")]
         public void get_there_are_two_started_engines_more_one_doors_closed()
         {
             IEnumerable<IEngine> engines = this.vehicleStorage
@@ -48,8 +44,6 @@ namespace BusinessCore.Tests
         }
 
         [TestMethod]
-        [TestCategory("Soden")]
-        [TestCategory("Functional Programing")]
         public void get_sum_number_of_wheels_of_black_vehicles_with_enrollment_number_higher_to_100_is_6()
         {
             double pressure = this.vehicleStorage
@@ -61,8 +55,6 @@ namespace BusinessCore.Tests
         }
 
         [TestMethod]
-        [TestCategory("Soden")]
-        [TestCategory("Functional Programing")]
         public void get_serial_enrollment_from_white_vehicles_with_at_least_one_door_and_horsePower_adobe_500cv()
         {
             var vehicles = this.vehicleStorage
