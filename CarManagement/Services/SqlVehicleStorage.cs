b6bdb3771ddbd8e92cd36c1d5933d205d7770a17
Celiamg <celiamargil@gmail.com>
2019-02-08 11:16:29 +0100

    implement IVehicle get, without testing

--- CarManagement/Services/SqlVehicleStorage.cs
+++ CarManagement/Services/SqlVehicleStorage.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CarManagement.Core.Models;
+using CarManagement.Core.Models.DTOs;
 using CarManagement.Core.Services;
 
 namespace CarManagement.Services
@@ -35,22 +36,78 @@ namespace CarManagement.Services
 
         public IVehicle get(IEnrollment enrollment)
         {
-            string query = $@"
-                            SELECT id 
-                            FROM enrollment
-                            WHERE serial = '{enrollment.Serial}' 
-                            AND number = {enrollment.Number};";
-
-            int enrollmentId = executeScalarQuery(this.connectionString, query);
+            VehicleDto vehicleDto = new VehicleDto();
+            string query;
 
             query = $@"
-                     SELECT * 
+                     SELECT serial
+                     FROM enrollment
+                     WHERE serial = '{enrollment.Serial}' 
+                     AND number = {enrollment.Number};";
+            string serial = executeScalarQuery(this.connectionString, query).ToString();
+            vehicleDto.Enrollment.Serial = serial;
+
+            query = $@"
+                     SELECT number
+                     FROM enrollment
+                     WHERE serial = '{enrollment.Serial}' 
+                     AND number = {enrollment.Number};";
+            int number = (int) executeScalarQuery(this.connectionString, query);
+            vehicleDto.Enrollment.Number = number;
+
+            query = $@"
+                     SELECT id
+                     FROM enrollment
+                     WHERE serial = '{enrollment.Serial}' 
+                     AND number = {enrollment.Number};";
+            int enrollmentId = (int)executeScalarQuery(this.connectionString, query);
+
+            query = $@"
+                     SELECT engineIsStarted 
                      FROM vehicle
                      WHERE enrollmentId = '{enrollmentId}';";
+            bool isStarted = (bool) executeScalarQuery(this.connectionString, query);
+            vehicleDto.Engine.IsStarted = isStarted;
 
-            //enrollmentId = executeScalarQuery(this.connectionString, query);
+            query = $@"
+                     SELECT engineHorsePower
+                     FROM vehicle
+                     WHERE enrollmentId = '{enrollmentId}';";
+            int horsePower = (int) executeScalarQuery(this.connectionString, query);
+            vehicleDto.Engine.HorsePower = horsePower;
 
+            query = $@"
+                     SELECT color
+                     FROM vehicle
+                     WHERE enrollmentId = '{enrollmentId}';";
+            CarColor color = (CarColor) executeScalarQuery(this.connectionString, query);
+            vehicleDto.Color = color;
 
+            query = $@"
+                     SELECT presure
+                     FROM wheel
+                     WHERE id = '{enrollmentId}';";
+            double[] pressure = (double[]) executeScalarQuery(this.connectionString, query);
+            int i = 0;
+            foreach(WheelDto wheelDto in vehicleDto.Wheels)
+            {
+                wheelDto.Pressure = pressure[i];
+                i++;
+            }
+
+            query = $@"
+                     SELECT isOpen
+                     FROM door
+                     WHERE id = '{enrollmentId}';";
+            bool[] isOpen = (bool[])executeScalarQuery(this.connectionString, query);
+            i = 0;
+            foreach (DoorDto doorDto in vehicleDto.Doors)
+            {
+                doorDto.IsOpen = isOpen[i];
+                i++;
+            }
+
+            return this.vehicleBuilder.import(vehicleDto);
         }
 
         public IEnumerable<IVehicle> getAll()
@@ -77,16 +134,16 @@ namespace CarManagement.Services
             }
         }
 
-        private static int executeScalarQuery(string connectionString, string query)
+        private static object executeScalarQuery(string connectionString, string query)
         {
-            int result;
+            object result = null;
 
             SqlConnection connection = new SqlConnection(connectionString);
             connection.Open();
             SqlCommand command = new SqlCommand(query, connection);
 
             result = (int)command.ExecuteScalar();
-
+ 
             connection.Close();
 
             return result;
