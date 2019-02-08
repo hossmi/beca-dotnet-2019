f007e020313b7e59fb8e531f5177abe5dfaffeee
Celiamg <celiamargil@gmail.com>
2019-02-08 10:17:07 +0100

    Add functions executeCommand and executeScalarQuery

--- CarManagement/Services/SqlVehicleStorage.cs
+++ CarManagement/Services/SqlVehicleStorage.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,7 +33,22 @@ namespace CarManagement.Services
 
         public IVehicle get(IEnrollment enrollment)
         {
-            throw new NotImplementedException();
+            string query = $@"
+                            SELECT id 
+                            FROM enrollment
+                            WHERE serial = '{enrollment.Serial}' 
+                            AND number = {enrollment.Number};";
+
+            int enrollmentId = executeScalarQuery(this.connectionString, query);
+
+            query = $@"
+                     SELECT * 
+                     FROM vehicle
+                     WHERE enrollmentId = '{enrollmentId}';";
+
+            //enrollmentId = executeScalarQuery(this.connectionString, query);
+
+
         }
 
         public IEnumerable<IVehicle> getAll()
@@ -42,6 +59,35 @@ namespace CarManagement.Services
         public void set(IVehicle vehicle)
         {
             throw new NotImplementedException();
+        }
+
+        private static void executeCommand(string connectionString, string sentencies)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            using (IDbCommand command = new SqlCommand())
+            {
+                command.CommandText = sentencies;
+                command.Connection = connection;
+
+                connection.Open();
+                int afectedRows = command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        private static int executeScalarQuery(string connectionString, string query)
+        {
+            int result;
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+
+            result = (int)command.ExecuteScalar();
+
+            connection.Close();
+
+            return result;
         }
     }
 }
