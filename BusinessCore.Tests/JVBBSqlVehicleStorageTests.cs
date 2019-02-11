bd8c61417145addd2728c028b5b378bca3df44d6
jvbb <jvbb@hotmail.es>
2019-02-11 11:58:18 +0100

    Add System.Data.dll and JVBBSqlVehicleStorageTests

--- BusinessCore.Tests/JVBBSqlVehicleStorageTests.cs
+++ BusinessCore.Tests/JVBBSqlVehicleStorageTests.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.IO;
-using System.Linq;
 using BusinessCore.Tests.Services;
 using CarManagement.Core.Models;
 using CarManagement.Core.Services;
@@ -19,6 +20,16 @@ namespace BusinessCore.Tests
         private const string CREATION_SCRIPT_FILE_KEY = "CreationScriptFile";
         private const string DESTRUCTION_SCRIPT_FILE_KEY = "DestructionScriptFile";
         private const string SCRIPTS_FOLDER_KEY = "scriptsFolder";
+
+        private const string INSERT_ENROLLMENT = "INSERT INTO [enrollment] (serial,number) " +
+            "OUTPUT INSERTED.ID " +
+            "VALUES (@serial, @number)";
+        private const string INSERT_VEHICLE = "INSERT INTO [vehicle] (enrollmentId, color, engineHorsePower, engineIsStarted) " +
+            "VALUES (@enrollmentKEY, @color, @horsepower, @started)";
+        private const string INSERT_WHEEL = "INSERT INTO [wheel] (pressure,vehicleId) " +
+            "VALUES (@pressure, @enrollmentKEY)";
+        private const string INSERT_DOOR = "INSERT INTO [door] (isOpen, vehicleId) " +
+            "VALUES (@open, @enrollmentKEY)";
 
         private readonly string connectionString;
         private readonly string creationScript;
@@ -102,7 +113,7 @@ namespace BusinessCore.Tests
         [TestMethod]
         public void there_are_ten_vehicles_stored_at_database_using_Count()
         {
-            IVehicleStorage databaseVehicleStorage = 
+            IVehicleStorage databaseVehicleStorage =
                 new SqlVehicleStorage(this.connectionString, this.vehicleBuilder);
 
             Assert.AreEqual(10, databaseVehicleStorage.Count);
@@ -121,17 +132,70 @@ namespace BusinessCore.Tests
 
         private static void fullfillWithSampleData(string connectionString, IEnumerable<IVehicle> vehicles)
         {
-            throw new NotImplementedException();
+            int insertedVehicles = 0;
+            int insertedWheels = 0;
+            int insertedDoors = 0;
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+
+            foreach (IVehicle vehicle in vehicles)
+            {
+                SqlCommand sqlCommand = new SqlCommand(INSERT_ENROLLMENT, sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@serial", vehicle.Enrollment.Serial);
+                sqlCommand.Parameters.AddWithValue("@number", vehicle.Enrollment.Number);
+                string enrollmentKEY = sqlCommand.ExecuteScalar().ToString();
+
+                sqlCommand = new SqlCommand(INSERT_VEHICLE, sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@enrollmentKEY", enrollmentKEY);
+                sqlCommand.Parameters.AddWithValue("@color", ((int)vehicle.Color));
+                sqlCommand.Parameters.AddWithValue("@horsepower", vehicle.Engine.HorsePower);
+                sqlCommand.Parameters.AddWithValue("@started", Convert.ToInt32(vehicle.Engine.IsStarted));
+                insertedVehicles = insertedVehicles + sqlCommand.ExecuteNonQuery();
+
+                foreach (IWheel wheel in vehicle.Wheels)
+                {
+                    sqlCommand = new SqlCommand(INSERT_WHEEL, sqlConnection);
+                    sqlCommand.Parameters.AddWithValue("@pressure", wheel.Pressure);
+                    sqlCommand.Parameters.AddWithValue("@enrollmentKEY", enrollmentKEY);
+                    insertedWheels = insertedWheels + sqlCommand.ExecuteNonQuery();
+                }
+
+                foreach (IDoor door in vehicle.Doors)
+                {
+                    sqlCommand = new SqlCommand(INSERT_DOOR, sqlConnection);
+                    sqlCommand.Parameters.AddWithValue("@open", Convert.ToInt32(door.IsOpen));
+                    sqlCommand.Parameters.AddWithValue("@enrollmentKEY", enrollmentKEY);
+                    insertedDoors = insertedDoors + sqlCommand.ExecuteNonQuery();
+                }
+            }
+            sqlConnection.Close();
         }
 
         private static void create(string connectionString, string creationScript)
         {
-            throw new NotImplementedException();
+            string script = File.ReadAllText(creationScript);
+            executeNonQueryCommand(connectionString, script);
         }
 
         private static void drop(string connectionString, string destructionScript)
         {
-            throw new NotImplementedException();
+            string script = File.ReadAllText(destructionScript);
+            executeNonQueryCommand(connectionString, script);
+        }
+
+        private static void executeNonQueryCommand(string connectionString, string script)
+        {
+            using (IDbConnection sqlConnection = new SqlConnection(connectionString))
+            using (IDbCommand sqlCommand = new SqlCommand())
+            {
+                sqlCommand.CommandText = script;
+                sqlCommand.Connection = sqlConnection;
+
+                sqlConnection.Open();
+                int affectedRows = sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
         }
 
     }
