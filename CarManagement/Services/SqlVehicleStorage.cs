6ee3bcec49391f7c1ee7fd4d01ae735ed1403e7f
EpicSimple <anton_the@hotmail.es>
2019-02-11 12:59:38 +0100

    Implemented SqlVehicleStorafe.get() and readied .set()

--- CarManagement/Services/SqlVehicleStorage.cs
+++ CarManagement/Services/SqlVehicleStorage.cs
@@ -13,6 +13,10 @@ namespace CarManagement.Services
 {
     public class SqlVehicleStorage : IVehicleStorage
     {
+        const string QUERY_VEHICLE_SKEL = "SELECT * FROM vehicle WHERE enrollmentId=@id";
+        const string QUERY_WHEEL_SKEL = "SELECT * FROM wheel WHERE vehicleId=@id";
+        const string QUERY_DOOR_SKEL = "SELECT * FROM door WHERE vehicleId=@id";
+
         private readonly string connectionString;
         private readonly IVehicleBuilder vehicleBuilder;
 
@@ -54,15 +58,75 @@ namespace CarManagement.Services
 
         public IVehicle get(IEnrollment enrollment)
         {
-            throw new NotImplementedException();
+            const string QUERY_ENROLL_SKEL = "SELECT * FROM enrollment WHERE serial=@serial AND number=@number";
+
+            using (SqlConnection sqlDbConnection = new SqlConnection(this.connectionString))
+            {
+                SqlCommand querier = new SqlCommand(QUERY_ENROLL_SKEL, sqlDbConnection);
+                sqlDbConnection.Open();
+
+                using (SqlDataReader sqlReader = querier.ExecuteReader())
+                {
+                    if(sqlReader.Read())
+                    {
+                        querier = new SqlCommand(QUERY_VEHICLE_SKEL, sqlDbConnection);
+                        querier.Parameters.AddWithValue("@id", (int)sqlReader["id"]);
+                        SqlDataReader sqlVehicleReader = querier.ExecuteReader();
+
+
+                        List<DoorDto> doorList = new List<DoorDto>();
+                        querier = new SqlCommand(QUERY_DOOR_SKEL, sqlDbConnection);
+                        querier.Parameters.AddWithValue("@id", (int)sqlReader["id"]);
+                        using (SqlDataReader sqlDoorReader = querier.ExecuteReader())
+                        {
+                            while (sqlDoorReader.Read())
+                            {
+                                doorList.Add(new DoorDto { IsOpen = Convert.ToBoolean(sqlDoorReader["isOpen"]) });
+                            }
+                        }
+
+                        List<WheelDto> wheelList = new List<WheelDto>();
+                        querier = new SqlCommand(QUERY_WHEEL_SKEL, sqlDbConnection);
+                        querier.Parameters.AddWithValue("@id", (int)sqlReader["id"]);
+                        using (SqlDataReader sqlWheelReader = querier.ExecuteReader())
+                        {
+                            while (sqlWheelReader.Read())
+                            {
+                                wheelList.Add(new WheelDto { Pressure = Convert.ToDouble(sqlWheelReader["pressure"]) });
+                            }
+                        }
+
+                        return this.vehicleBuilder
+                            .import(
+                                new VehicleDto
+                                {
+                                    Enrollment = new EnrollmentDto
+                                    {
+                                        Serial = sqlReader["serial"].ToString(),
+                                        Number = Convert.ToInt32(sqlReader["number"]),
+                                    },
+                                    Engine = new EngineDto
+                                    {
+                                        IsStarted = Convert.ToBoolean(sqlVehicleReader["engineIsStarted"]),
+                                        HorsePower = Convert.ToInt16(sqlVehicleReader["engineHorsePower"]),
+                                    },
+                                    Color = (CarColor)Convert.ToInt32(sqlVehicleReader["color"]),
+                                    Doors = doorList.ToArray(),
+                                    Wheels = wheelList.ToArray(),
+                                }
+                            );
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
         }
 
         public IEnumerable<IVehicle> getAll()
         {
             const string QUERY_ENROLL_SKEL = "SELECT * FROM enrollment";
-            const string QUERY_VEHICLE_SKEL = "SELECT * FROM vehicle WHERE enrollmentId=@id";
-            const string QUERY_WHEEL_SKEL = "SELECT * FROM wheel WHERE vehicleId=@id";
-            const string QUERY_DOOR_SKEL = "SELECT * FROM door WHERE vehicleId=@id";
 
             List<IVehicle> vehiclesToReturn = new List<IVehicle>();
 
@@ -159,7 +223,52 @@ namespace CarManagement.Services
 
         public void set(IVehicle vehicle)
         {
-            throw new NotImplementedException();
+            #region "SQL skels CONSTS"
+            const string QUERY_ENROLL_SKEL = "SELECT * FROM enrollment " +
+                "WHERE serial=@serial AND number=@number";
+
+            const string UPDATE_VEHICLE_SKEL = "UPDATE vehicle" +
+                "(color, engineIsStarted, engineHorsePower)" +
+                " VALUES (@color, @engineIsStarted, @engineHorsePower) WHERE enrollmentId=@id";
+            const string UPDATE_DOOR_SKEL = "UPDATE door(isOpen)" +
+                " VALUES (@isOpen) WHERE vehicleId=@id";
+            const string UPDATE_WHEEL_SKEL = "UPDATE vehicle(pressure)" +
+                " VALUES (@pressure) WHERE vehicleId=@id";
+
+            const string INSERT_ENROLL_SKEL = "INSERT INTO enrollment " +
+                "(serial, number)" +
+                " output INSERTED.ID VALUES " +
+                "(@serial, @number)";
+            const string INSERT_VEHICLE_SKEL = "INSERT INTO vehicle" +
+                " (enrollmentId, color, engineHorsePower, engineIsStarted)" +
+                " VALUES (@enrollmentId, @color, @engineHorsePower, @engineIsStarted)";
+            const string INSERT_DOOR_SKEL = "INSERT INTO door " +
+                "(vehicleId, isOpen) " +
+                "VALUES (@vehicleId, @isOpen)";
+            const string INSERT_WHEEL_SKEL = "INSERT INTO wheel " +
+                "(vehicleId, pressure) " +
+                "VALUES (@vehicleId, @pressure)";
+            #endregion
+
+            using (SqlConnection sqlDbConnection = new SqlConnection(this.connectionString))
+            {
+                SqlCommand querier = new SqlCommand(QUERY_ENROLL_SKEL, sqlDbConnection);
+                sqlDbConnection.Open();
+
+                using (SqlDataReader sqlReader = querier.ExecuteReader())
+                {
+                    if (sqlReader.Read())
+                    {
+                        querier = new SqlCommand(UPDATE_VEHICLE_SKEL, sqlDbConnection);
+                        querier.Parameters.AddWithValue("@id", (int)sqlReader["id"]);
+                        SqlDataReader sqlVehicleReader = querier.ExecuteReader();
+                    }
+                    else
+                    {
+                        //insert it
+                    }
+                }
+            }
         }
     }
 }
