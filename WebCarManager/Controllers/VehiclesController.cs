7665c9cc42567eaf746e583a633f5ff1c875e42f
Necoleto <xyztresd@gmail.com>
2019-02-25 13:18:02 +0100

    Update Start work on web aplication

---
+++ WebCarManager/Controllers/VehiclesController.cs
@@ -0,0 +1,35 @@
+﻿using CarManagement.Core.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCarManager.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        // GET: Vehicles
+        public ActionResult Index()
+        {
+            VehicleDto[] vehicles = new VehicleDto[]
+            {
+                new VehicleDto
+                {
+                    Enrollment = new EnrollmentDto()
+                    {
+                        Serial = "AAAA",
+                        Number = 0000
+
+                    },
+                    Color = CarManagement.Core.Models.CarColor.Green
+                }
+            };
+            return View(vehicles);
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
