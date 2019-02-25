16c07877dc9482de16854b660e2e01596f163839
Soden <f.navarronicolas@gmail.com>
2019-02-25 14:59:16 +0100

    Working... moving equipment to the multi-room

--- WebCarManager/Controllers/EnrollmentsController.cs
+++ WebCarManager/Controllers/EnrollmentsController.cs
@@ -5,15 +5,17 @@ using System.Web;
 using System.Web.Mvc;
 using CarManagement.Services;
 using CarManagement.Core.Services;
-
+using System.Configuration;
 
 namespace WebCarManager.Controllers
 {
     public class EnrollmentsController : Controller
     {
         // GET: Enrollments
+        private readonly connectionString = "CarManagerConnectionString";
         public ActionResult Index()
         {
+            this.connectionString = ConfigurationManager.AppSettings[CONNECTION_STRING_KEY];
             IEnrollmentProvider enrollmentProvider = new DefaultEnrollmentProvider();
             VehicleBuilder vehicleBuilder = new VehicleBuilder(enrollmentProvider);
             SqlVehicleStorage enrollments = new SqlVehicleStorage();
