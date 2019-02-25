db4b87e6e46252769be08762088bff6214c5d583
Soden <f.navarronicolas@gmail.com>
2019-02-25 13:20:07 +0100

    add name Vehicles with functional link in the floating top menu

---
+++ WebCarManager/Controllers/VehiclesController.cs
@@ -0,0 +1,31 @@
+﻿using CarManagement.Core.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCarManager.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        // GET: Vehicles
+        public ActionResult Index()
+        {
+            VehicleDto[] vehicleDtos = new VehicleDto[]
+            {
+                new VehicleDto
+                {
+                     Enrollment= new EnrollmentDto()
+                     {
+                          Number=666,
+                           Serial="BBB"
+                     },
+                      Color = CarManagement.Core.Models.CarColor.Black
+                }
+            };
+
+            return View(vehicleDtos);
+        }
+    }
+}
