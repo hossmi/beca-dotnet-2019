fdc68ba5758e205fd0ed4ff99433523f00de19cc
adrianla <the_adrianin23@hotmail.com>
2019-02-25 13:09:50 +0100

    no message

---
+++ WebCarManager/Controllers/VehiclesController.cs
@@ -0,0 +1,30 @@
+﻿using CarManagement.Core.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCarManager.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        // GET: Vehicles
+        public ActionResult Index()
+        {
+            VehicleDto[] vehicles = new VehicleDto[]
+            {
+            new VehicleDto
+            {
+                Enrollment =  new EnrollmentDto
+                {Serial = "XXX",
+                Number=666,
+                }
+            }
+        };
+        
+            
+            return View(vehicles);
+        }
+    }
+}
