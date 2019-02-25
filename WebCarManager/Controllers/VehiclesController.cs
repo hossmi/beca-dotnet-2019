96677ce7a0379d33e5ad5d6b84437c1167d475e7
evoblackshark <kilowan2@gmail.com>
2019-02-25 14:58:08 +0100

    commit web

--- WebCarManager/Controllers/VehiclesController.cs
+++ WebCarManager/Controllers/VehiclesController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using CarManagement.Core.Models;
 using CarManagement.Core.Models.DTOs;
 
 namespace WebCarManager.Controllers
@@ -17,7 +18,25 @@ namespace WebCarManager.Controllers
                         Serial= "XXX",
                         Number = 666,
                     },
+                    Engine = new EngineDto
+                    {
+                        HorsePower = 100,
+                        IsStarted = true
+                    },
                     Color = CarManagement.Core.Models.CarColor.Red,
+                    Doors = new DoorDto[]
+                    {
+                        new DoorDto { IsOpen = true },
+                        new DoorDto { IsOpen = false },
+                    },
+                    Wheels = new WheelDto[]
+                    {
+                        new WheelDto{Pressure = 4},
+                        new WheelDto{Pressure = 4},
+                        new WheelDto{Pressure = 2},
+                        new WheelDto{Pressure = 2},
+                    },
+
                 }
             };
 
