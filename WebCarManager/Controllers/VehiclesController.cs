257d92c805df457792138f482f632cc252b29073
Soden <f.navarronicolas@gmail.com>
2019-02-26 15:23:25 +0100

    add name change driver EnrollmentController by VehicleController

--- WebCarManager/Controllers/VehiclesController.cs
+++ WebCarManager/Controllers/VehiclesController.cs
@@ -1,28 +1,57 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using CarManagement.Services;
+using CarManagement.Core.Services;
+using System.Configuration;
+using CarManagement.Core.Models;
 using CarManagement.Core.Models.DTOs;
+using System.Linq;
 
 namespace WebCarManager.Controllers
 {
     public class VehiclesController : Controller
     {
-        
-        // GET: Vehicles
+        // GET: Enrollments
+        private const string CONNECTION_STRING_KEY = "CarManagerConnectionString";
+        private string connectionString;
+
         public ActionResult Index()
         {
-            VehicleDto[] vehicles = new VehicleDto[]
-            {
-                new VehicleDto
-                {
-                    Enrollment = new EnrollmentDto
-                    {
-                        Serial= "XXX",
-                        Number = 666,
-                    },
-                    Color = CarManagement.Core.Models.CarColor.Red,
-                }
-            };
+            this.connectionString = ConfigurationManager.AppSettings[CONNECTION_STRING_KEY];
+            IEnrollmentProvider enrollmentProvider = new DefaultEnrollmentProvider();
+            IVehicleBuilder vehicleBuilder = new VehicleBuilder(enrollmentProvider);
+            IVehicleStorage vehicleStorage = new SqlVehicleStorage(this.connectionString, vehicleBuilder);
 
-            return View(vehicles);
+            IEnumerable<IEnrollment> enrollments = vehicleStorage.get().Keys;
+
+            return View(enrollments);
+        }
+
+
+        public ActionResult Details(string serial, int number)
+        {
+            VehicleDto vehicleDto = getVehicleDto(serial, number, CONNECTION_STRING_KEY);
+
+            return View(vehicleDto);
+        }
+
+        private static VehicleDto getVehicleDto(string serial, int number, string connection)
+        {
+            String connectionString = ConfigurationManager.AppSettings[connection];
+            IEnrollmentProvider enrollmentProvider = new DefaultEnrollmentProvider();
+            IVehicleBuilder vehicleBuilder = new VehicleBuilder(enrollmentProvider);
+            IVehicleStorage vehicleStorage = new SqlVehicleStorage(connectionString, vehicleBuilder);
+
+            IEnrollment enrollment = enrollmentProvider.import(serial, number);
+
+            IVehicle vehicle = vehicleStorage.get().whereEnrollmentIs(enrollment).Single();
+
+            VehicleDto vehicleDto = vehicleBuilder.export(vehicle);
+
+
+            return vehicleDto;
         }
     }
 }
