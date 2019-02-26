2008f71cea491ced92a633227fb0d6ef6853fcb2
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2019-02-26 13:18:30 +0100

    implement index and view actions from VehicleController

--- WebCarManager/Controllers/VehiclesController.cs
+++ WebCarManager/Controllers/VehiclesController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using CarManagement.Core.Models;
 using CarManagement.Core.Models.DTOs;
 using CarManagement.Core.Services;
 
@@ -9,29 +10,42 @@ namespace WebCarManager.Controllers
     public class VehiclesController : AbstractController
     {
         private readonly IVehicleStorage vehicleStorage;
+        private readonly IEnrollmentImporter enrollmentImporter;
+        private readonly IVehicleBuilder vehicleBuilder;
 
         public VehiclesController()
         {
             this.vehicleStorage = getService<IVehicleStorage>();
+            this.enrollmentImporter = getService<IEnrollmentImporter>();
+            this.vehicleBuilder = getService<IVehicleBuilder>();
         }
 
         // GET: Vehicles
         public ActionResult Index()
         {
-            VehicleDto[] vehicles = new VehicleDto[]
-            {
-                new VehicleDto
-                {
-                    Enrollment = new EnrollmentDto
-                    {
-                        Serial= "XXX",
-                        Number = 666,
-                    },
-                    Color = CarManagement.Core.Models.CarColor.Red,
-                }
-            };
+            IEnumerable<IEnrollment> vehicles = this.vehicleStorage
+                .get()
+                .Keys;
 
             return View(vehicles);
+        }
+
+        public ActionResult  View(string serial, int number)
+        {
+            IEnrollment enrollment = this.enrollmentImporter.import(serial, number);
+
+            VehicleDto vehicle = this.vehicleStorage
+                .get()
+                .whereEnrollmentIs(enrollment)
+                .Select(this.vehicleBuilder.export)
+                .SingleOrDefault();
+
+            if (vehicle == null)
+            {
+                return RedirectToAction("index");
+            }
+            else
+                return View(vehicle);
         }
     }
 }
