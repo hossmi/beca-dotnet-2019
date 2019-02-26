be5183cad1f096994294e9fcb1f8ab400576c9af
Celiamg <celiamargil@gmail.com>
2019-02-26 10:51:41 +0100

    Create View Detalles

--- WebCarManager/Controllers/VehiclesController.cs
+++ WebCarManager/Controllers/VehiclesController.cs
@@ -1,6 +1,10 @@
-﻿using CarManagement.Core.Models.DTOs;
+﻿using CarManagement.Core.Models;
+using CarManagement.Core.Models.DTOs;
+using CarManagement.Core.Services;
+using CarManagement.Services;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,6 +13,7 @@ namespace WebCarManager.Controllers
 {
     public class VehiclesController : Controller
     {
+        private const string CONNECTION_STRING = "CarManagerConnectionString";
         // GET: Vehicles
         public ActionResult Index()
         {
@@ -21,11 +26,22 @@ namespace WebCarManager.Controllers
                         Serial = "XXX",
                         Number = 6666,
                     },
-                    Color = CarManagement.Core.Models.CarColor.Black,
+                    Color = CarColor.Black,
                 },
                 
             };
 
+            return View(vehicles);
+        }
+
+        public ActionResult Detalles()
+        {
+            string connectionString = ConfigurationManager.AppSettings[CONNECTION_STRING];
+            IEnrollmentProvider enrollmentProvider = new DefaultEnrollmentProvider();
+            IVehicleBuilder vehicleBuilder = new VehicleBuilder(enrollmentProvider);
+            IVehicleStorage vehicleStorage = new SqlVehicleStorage(connectionString, vehicleBuilder);
+            IEnumerable<IVehicle> vehicles = vehicleStorage.get();
+            //vehicles.Select(vehicle => vehicleStorage.get().whereEnrollmentIs());
             return View(vehicles);
         }
     }
