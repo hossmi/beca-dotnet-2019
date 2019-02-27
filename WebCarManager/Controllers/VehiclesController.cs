d8a1a2e5483a61f06e4e9221b900f822f536737e
EpicSimple <anton_the@hotmail.es>
2019-02-27 15:42:39 +0100

    Navigation implemented, crash on submit new or edited vehicle, too.

--- WebCarManager/Controllers/VehiclesController.cs
+++ WebCarManager/Controllers/VehiclesController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using CarManagement.Core.Models;
 using CarManagement.Core.Models.DTOs;
@@ -10,10 +11,14 @@ namespace WebCarManager.Controllers
     public class VehiclesController : AbstractController
     {
         private readonly IVehicleStorage vehicleStorage;
+        private readonly IVehicleBuilder vehicleBuilder;
+        private readonly IEnrollmentProvider enrollmentProvider;
 
         public VehiclesController()
         {
             this.vehicleStorage = getService<IVehicleStorage>();
+            this.vehicleBuilder = getService<IVehicleBuilder>();
+            this.enrollmentProvider = getService<IEnrollmentProvider>();
         }
 
         // GET: Vehicles
@@ -26,22 +31,80 @@ namespace WebCarManager.Controllers
 
         public ActionResult Create()
         {
-            return View();
+            return View(new VehicleDto { Engine= new EngineDto { HorsePower= 50, IsStarted= false, } });
+        }
+        [HttpPost]
+        public ActionResult Created(VehicleDto vehicleDto)
+        {
+            return View(vehicleDto);
         }
 
         public ActionResult Edit(string serial, int number)
         {
-            return View();
+            VehicleDto vehicle = getVehicleAsDto(serial, number);
+
+            return View(vehicle);
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(VehicleDto vehicle)
+        {
+            return View(vehicle);
         }
 
         public ActionResult Details(string serial, int number)
         {
-            return View();
+            VehicleDto vehicle = getVehicleAsDto(serial, number);
+            return View(vehicle);
         }
 
         public ActionResult Delete(string serial, int number)
         {
-            return View();
+
+            return View
+                (
+                this.enrollmentProvider
+                    .import
+                    (
+                        serial,
+                        number
+                    )
+                );
+        }
+        [HttpPost]
+        public ActionResult Deleted(string serial, int number)
+        {
+
+            return View
+                (
+                this.enrollmentProvider
+                    .import
+                    (
+                        serial,
+                        number
+                    )
+                );
+        }
+
+        private VehicleDto getVehicleAsDto(string serial, int number)
+        {
+            return this.vehicleBuilder
+            .export
+            (
+                this.vehicleStorage
+                .get()
+                .whereEnrollmentIs
+                (
+                    this.enrollmentProvider
+                    .import
+                    (
+                        serial,
+                        number
+                    )
+                )
+                .Single()
+            );
         }
     }
 }
