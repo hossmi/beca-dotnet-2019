f747db30d183ff0585d3cf4cc1640c723fba47b4
alloret1993 <alloret1993@outlook.com>
2019-02-27 13:30:04 +0100

    views create and delete

--- WebCarManager/Controllers/VehiclesController.cs
+++ WebCarManager/Controllers/VehiclesController.cs
@@ -6,7 +6,6 @@ using CarManagement.Core.Models.DTOs;
 using CarManagement.Core.Services;
 using CarManagement.Services;
 
-
 namespace WebCarManager.Controllers
 {
     public class VehiclesController : AbstractController
@@ -22,6 +21,7 @@ namespace WebCarManager.Controllers
             this.vehicleBuilder = getService<IVehicleBuilder>();
         }
 
+        // GET: Vehicles
         public ActionResult Prueba()
         {
             List<EnrollmentDto> enrollments = new List<EnrollmentDto>();
@@ -37,7 +37,6 @@ namespace WebCarManager.Controllers
 
             return View(enrollments);
         }
-
 
         public ActionResult Details(string serial, int number)
         {
@@ -55,8 +54,8 @@ namespace WebCarManager.Controllers
 
         public ActionResult Create()
         {
-            VehicleDto vehicleDto = new VehicleDto();
-            return View(vehicleDto);
+            NewVehicleData vehicleData = new NewVehicleData();
+            return View(vehicleData);
         }
 
         public ActionResult Delete(string serial, int number)
@@ -74,17 +73,41 @@ namespace WebCarManager.Controllers
         {
             IEnrollment enrollment = this.enrollmentProvider.import(enrollmentDto.Serial, enrollmentDto.Number);
             this.vehicleStorage.remove(enrollment);
-            return RedirectToAction("Prueba");
+            return RedirectToAction("Index");
             //return View(enrollmentDto);
         }
 
 
         [HttpPost]
-        public ActionResult Create(VehicleDto vehicleDto)
+        public ActionResult Create(NewVehicleData vehicleData)
         {
-            SetVehicleData(vehicleDto);
+            IVehicle vehicle;
+            VehicleBuilder builder = new VehicleBuilder(this.enrollmentProvider);
 
-            return View(vehicleDto);
+            builder.setColor(vehicleData.Color);
+            builder.setEngine(vehicleData.HorsePower);
+            builder.setDoors(vehicleData.DoorCount);
+
+            for (int i = 0; i < vehicleData.WheelCount; i++)
+            {
+                builder.addWheel();
+            }
+
+            vehicle = builder.build();
+
+            if (vehicleData.IsStarted == true)
+            {
+                vehicle.Engine.start();
+            }
+
+            foreach (IWheel wheel in vehicle.Wheels)
+            {
+                wheel.Pressure = vehicleData.Pressure;
+            }
+
+            this.vehicleStorage.set(vehicle);
+
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
@@ -109,21 +132,25 @@ namespace WebCarManager.Controllers
             this.vehicleStorage.set(vehicle);
         }
 
-        private class NewVehicleData
+        public class NewVehicleData
         {
             private int wheelCount;
             private int doorCount;
             private int horsePower;
             private bool isStarted;
             private CarColor color;
+            private double pressure;
+
+            public int WheelCount { get => this.wheelCount; set => this.wheelCount = value; }
+            public int DoorCount { get => this.doorCount; set => this.doorCount = value; }
+            public int HorsePower { get => this.horsePower; set => this.horsePower = value; }
+            public bool IsStarted { get => this.isStarted; set => this.isStarted = value; }
+            public CarColor Color { get => this.color; set => this.color = value; }
+            public double Pressure { get => this.pressure; set => this.pressure = value; }
+
         }
 
-        //continuar en casa
+
 
     }
 }
-
-
-
-
-
