a2cde126097e7fdb4ecfc45a9e876a538adc7c41
evoblackshark <kilowan2@gmail.com>
2019-03-08 09:58:08 +0100

    Clean VehiclesController

--- WebCarManager/Controllers/VehiclesController.cs
+++ WebCarManager/Controllers/VehiclesController.cs
@@ -23,18 +23,19 @@ namespace WebCarManager.Controllers
             this.vehicleStorage = getService<IVehicleStorage>();
             this.enrollmentProvider = new DefaultEnrollmentProvider();
             this.vehicleBuilder = new VehicleBuilder(this.enrollmentProvider);
-            this.enrollmentDto = new EnrollmentDto();
             this.vehicleDto = new VehicleDto();
-            this.enrollmentDtoList = new List<EnrollmentDto>();
-            this.enrollmentEnum = this.vehicleStorage
-                .get()
-                .Keys;
         }
 
         public ActionResult Index()
         {
+            this.enrollmentDtoList = new List<EnrollmentDto>();
+            this.enrollmentEnum = this.vehicleStorage
+                .get()
+                .Keys;
             foreach (IEnrollment enrollment in this.enrollmentEnum)
             {
+                this.enrollmentDto = new EnrollmentDto();
+                //this.vehicleBuilder.export(enrollment);
                 this.enrollmentDto = this.vehicleBuilder.export(enrollment);
                 this.enrollmentDtoList.Add(this.enrollmentDto);
             }
@@ -45,7 +46,6 @@ namespace WebCarManager.Controllers
         [HttpGet]
         public ActionResult Edit(string serial, int number)
         {
-            this.vehicleDto = new VehicleDto();
             this.vehicleDto = getVehicleData(serial, number);
             return View(this.vehicleDto);
         }
@@ -53,34 +53,16 @@ namespace WebCarManager.Controllers
         [HttpGet]
         public ActionResult Save(VehicleDto vehicleDto)
         {
-            this.vehicleStorage.set(convert(vehicleDto));
+            this.vehicleStorage.set(this.vehicleBuilder.import(vehicleDto));
             return View(getVehicleData(vehicleDto.Enrollment.Serial, vehicleDto.Enrollment.Number));
         }
 
         private VehicleDto getVehicleData(string serial, int number)
         {
-            return convert(selectIvehicle(this.vehicleStorage, convert(serial, number)));
-        }
-        private static IVehicle selectIvehicle(IVehicleStorage vehicleStorage, IEnrollment enrollment)
-        {
-            return vehicleStorage.get()
-            .whereEnrollmentIs(enrollment)
+            return this.vehicleDto = this.vehicleBuilder.export(this.vehicleStorage.get()
+            .whereEnrollmentIs(this.enrollmentProvider.import(serial, number))
             .Select(vehicle => vehicle)
-            .Single();
-        }
-        private IEnrollment convert(string serial, int number)
-        {
-            return this.enrollmentProvider.import(serial, number);
-        }
-        private VehicleDto convert(IVehicle vehicle)
-        {
-            this.vehicleDto = this.vehicleBuilder.export(vehicle);
-            return this.vehicleDto;
-        }
-        private IVehicle convert(VehicleDto vehicleDto)
-        {
-            this.vehicleBuilder = new VehicleBuilder(this.enrollmentProvider);
-            return this.vehicleBuilder.import(vehicleDto);
+            .Single());
         }
     }
 }
