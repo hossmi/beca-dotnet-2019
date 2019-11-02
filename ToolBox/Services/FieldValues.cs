2e4cddf3b059bffd7becf7e01080b6ace95e730d
evoblackshark <kilowan2@gmail.com>
2019-11-03 00:02:25 +0100

    add New object

---
+++ ToolBox/Services/FieldValues.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolBox.Services
+{
+    public class FieldValues
+    {
+        public string field { get; set; }
+        public IList<string> values { get; set; }
+    }
+}
