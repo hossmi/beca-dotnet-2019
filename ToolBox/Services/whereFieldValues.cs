93ae5f86e1826fab48b88f7416daf7c34ba7f856
evoblackshark <kilowan2@gmail.com>
2019-11-03 22:10:26 +0100

    Add new object

---
+++ ToolBox/Services/whereFieldValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolBox.Services
+{
+    public class whereFieldValues : FieldValues
+    {
+        public string key { get; set; }
+    }
+}
