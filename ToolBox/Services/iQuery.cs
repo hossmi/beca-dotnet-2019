a9913bffafecc19faab6f794c9a2fb44c4c1966b
evoblackshark <kilowan2@gmail.com>
2019-11-05 22:27:37 +0100

    add new object

---
+++ ToolBox/Services/iQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ToolBox.Services
+{
+    public class iQuery
+    {
+        public IList<FieldValues> tablesColumns { get; set; }
+        public IList<whereFieldValues> whereValues { get; set; }
+    }
+}
