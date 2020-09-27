112f3579002a246f13e19b05180b0597ea5c7722
PC-GAMER\Juan Francisco <kilowan2@gmail.com>
2020-09-27 19:48:53 +0200

    improve enrollmentProvider

--- CarManagement/Services/DefaultEnrollmentProvider.cs
+++ CarManagement/Services/DefaultEnrollmentProvider.cs
@@ -7,7 +7,7 @@ namespace CarManagement.Services
 {
     public class DefaultEnrollmentProvider : IEnrollmentProvider
     {
-        private StringBuilder A;
+        //private StringBuilder A;
         private StringBuilder Serial;
         private int lenght;
         private int Number;
@@ -17,8 +17,9 @@ namespace CarManagement.Services
         private int total;
         public DefaultEnrollmentProvider()
         {
-            this.A = new StringBuilder("BCDFGHJKLMNPRSTVWXYZ");
-            this.lenght = this.A.Length - 1;
+            //this.A = new StringBuilder("BCDFGHJKLMNPRSTVWXYZ");
+            //this.lenght = this.A.Length - 1;
+            this.lenght = Enum.GetNames(typeof(Letter)).Length - 1;
             this.Number = 0;
             this.i = 0;
             this.i2 = 0;
@@ -42,11 +43,35 @@ namespace CarManagement.Services
                 return $"{this.Serial}-{this.Number:0000}";
             }
         }
+        private enum Letter 
+        {
+            B = 0,
+            C = 1,
+            D = 2,
+            F = 3,
+            G = 4,
+            H = 5,
+            J = 6,
+            K = 7,
+            L = 8,
+            M = 9,
+            N = 10,
+            P = 11,
+            R = 12,
+            S = 13,
+            T = 14,
+            V = 15,
+            W = 16,
+            X = 17,
+            Y = 18,
+            Z = 19
+        }
 
         IEnrollment IEnrollmentProvider.getNew()
         {
             this.Serial = new StringBuilder(3);
-            this.Serial.Insert(0, $"{(this.A[this.i])}{(this.A[this.i2])}{(this.A[this.i3])}");
+            this.Serial.Append($"{(Letter)this.i}{(Letter)this.i2}{(Letter)this.i3}");
+            //this.Serial.Insert(0, $"{(this.A[this.i])}{(this.A[this.i2])}{(this.A[this.i3])}");
             if (this.i <= this.lenght)
             {
                 if (this.Number.Equals(this.total))
