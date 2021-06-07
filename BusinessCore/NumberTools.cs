a105f301a1a304dc7eb56651ff2ed4c6967a9861
PC-GAMER\Juan Francisco <kilowan2@gmail.com>
2021-06-07 21:09:07 +0200

    improve code

--- BusinessCore/NumberTools.cs
+++ BusinessCore/NumberTools.cs
@@ -18,22 +18,14 @@ namespace BusinessCore
         }
         public static int fibonacci(int n)
         {
-            if (n == 0 || n == 1)
+            int c = 0;
+            for (int a = 0, b = 1, i = 0; i < n; i++)
             {
-                return n;
+                c = a + b;
+                a = b;
+                b = c;
             }
-            else
-            {
-                int c = 0;
-                for (int i = 0, a = 0, b = 1; i < n - 1; i++)
-                {
-                    c = a + b;
-                    a = b;
-                    b = c;
-                }
-                return c;
-
-            }
+            return c;
         }
     }
 }
